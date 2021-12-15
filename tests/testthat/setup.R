3120b1dcf5f1a3919eb6f087c6abd6b01da2ec91
samatre <65082106+samatre@users.noreply.github.com>
2021-12-16 09:15:08 +1300

    Initial commit. Added testing data.

---
+++ tests/testthat/setup.R
@@ -0,0 +1,70 @@
+# Purpose : Test data for the sanity check functions in
+#           function_preprocess.R
+# Author  : Sameer Atre
+#           SAtre@abacusbio.co.nz
+
+# Everything here is executed before any test is run.
+# For data formats and required fields see the index testing app at
+# https://abacusbio.shinyapps.io/selection_index_revamp/
+
+# Normal'good'data.Has just the required columns and values.
+desc_bv <- read_xlsx(
+  "../data/description_bv.xlsx",
+  sheet = 1,
+  skip = 0,
+  col_types = rep("text", 2),
+  col_names = T,
+  trim_ws = T
+)
+
+# Normal'good'data but with optional fields.
+suppressMessages(
+  desc_bv_opt <- read_csv("../data/description_bv_optional.csv")
+  )
+
+
+# Has duplicate column headers.
+dup_desc_bv <- read_xlsx(
+  "../data/description_bv_duplicate_hdr.xlsx",
+  sheet = 1,
+  skip = 0,
+  col_types = rep("text", 2),
+  col_names = T,
+  trim_ws = T
+)
+
+# ACC-Trait mismatch.
+# Whereas every 'ACC' classifier should have a corresponding
+# 'TraitACC' entry in the column_label, this file has an ACC
+# classifier entries but does not have corresponding TraitACC
+# column_labels.
+test_desc_bv <-
+  read_xlsx(
+    "../data/description_bv_acc_trait_mismatch.xlsx",
+    sheet = 1,
+    skip = 0,
+    col_types = rep("text", 2),
+    col_names = T,
+    trim_ws = T
+  )
+
+# Has identical order values in trait label/EBV rows.
+suppressMessages(
+  desc_bv_identical_odr <- read_csv(
+    "../data/description_bv_identical_order.csv"
+    )
+  )
+
+# Has missing order values in trait label/EBV rows.
+suppressMessages(
+  desc_bv_missing_odr <- read_csv(
+    "../data/description_bv_missing_order.csv"
+    )
+  )
+
+# Has missing group values in trait label/EBV rows.
+suppressMessages(
+  desc_bv_missing_grp <- read_csv(
+    "../data/description_bv_missing_group.csv"
+    )
+  )
