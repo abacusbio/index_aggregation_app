98532b506c26e3fb4763d5fa383cc2f1afdbadc9
samatre <65082106+samatre@users.noreply.github.com>
2022-01-21 15:12:25 +1300

    set timeout for button click event.

--- R/tests/shinytest/test_demo_button.R
+++ R/tests/shinytest/test_demo_button.R
@@ -2,5 +2,5 @@ app <- ShinyDriver$new("../../", loadTimeout = 1e+04)
 app$snapshotInit("test_demo_button")
 
 app$snapshot(list(output = "step1-sanity_message"))
-app$setInputs(demo = "click")
+app$setInputs(demo = "click", timeout_ = 1e+04)
 app$snapshot(list(output = "step1-demo_message"))
