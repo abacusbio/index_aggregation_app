a1ca785ffe0c543fdeb282eb9b28702d9a485eea
samatre <65082106+samatre@users.noreply.github.com>
2022-01-21 14:24:25 +1300

    Test files purged

--- R/tests/shinytest/FilterEBV.R
+++
@@ -1,6 +0,0 @@
-app <- ShinyDriver$new("../../", loadTimeout = 1e+04)
-app$snapshotInit("FilterEBV")
-
-app$setInputs(demo = "click")
-app$setInputs(upload = "tab.step2")
-app$snapshot()
--- R/tests/shinytest/preprocessUploadMod-DemoData.R
+++
@@ -1,5 +0,0 @@
-app <- ShinyDriver$new("../../", loadTimeout = 1e+04)
-app$snapshotInit("preprocessUploadMod-DemoData")
-
-app$setInputs(demo = "click")
-app$snapshot()
